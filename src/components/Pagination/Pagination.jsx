1043429a4095e8b8793ee987ea9707ed8850669d
Wizuha <williams.wandji@gmail.com>
2024-09-17 14:56:40 +0200

    update pagination

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,28 +1,35 @@
-import React from 'react'
-import './Pagination.css'
+import React from "react";
+import "./Pagination.css";
 
 function Pagination({ totalPosts, postsPerPage, setCurrentPage, currentPage }) {
-  let pages = [];
+  const pages = [];
+  const maxButtons = 5;
 
+  const totalPages = Math.ceil(totalPosts / postsPerPage);
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
     pages.push(i);
   }
+  const getPaginationButtons = () => {
+    const start = Math.max(1, currentPage - Math.floor(maxButtons / 2));
+    const end = Math.min(totalPages, start + maxButtons - 1);
+    return pages.slice(start - 1, end);
+  };
+
   return (
     <div className="pagination">
-        {pages.map((page,index) => {
-                return (
-                  <button
-                    key={index}
-                    onClick={() => setCurrentPage(page)}
-                    className={page == currentPage ? "active" : ""}
-                  >
-                    {page}
-                  </button>
-                );
-            })
-        }
+      {getPaginationButtons().map((page, index) => {
+        return (
+          <button
+            key={index}
+            onClick={() => setCurrentPage(page)}
+            className={page == currentPage ? "active" : ""}
+          >
+            {page}
+          </button>
+        );
+      })}
     </div>
-  )
+  );
 }
 
-export default Pagination
+export default Pagination;
